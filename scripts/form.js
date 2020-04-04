542c04d0dc79afac59dddd559d330e76356992c7
mkarzel <karzelmarcin@gmail.com>
2020-04-04 16:52:14 +0200

    form update fixes

--- scripts/form.js
+++ scripts/form.js
@@ -1,16 +1,25 @@
+const heroesSelect = document.querySelector("#heroesSelect")
+
 const heroesToSelect = async () => {
     const heroes = await getHero(api);
 
     for (i = 0; i < heroes.length; i++) {
         const newOption = document.createElement("option")
         newOption.setAttribute("id","heroName")
-        document.querySelector("#heroesSelect").appendChild(newOption)
+        heroesSelect.appendChild(newOption)
         newOption.innerText = heroes[i].name
     }
 }
 
+const clearInputs = () => {
+    const inputs = document.querySelectorAll(".form__input")
+    for (i=0; i < inputs.length; i++){
+        inputs[i].value = null
+    }
+}
+
 window.onload = async () => {
-    if(document.querySelector("#heroesSelect")){
+    if(heroesSelect){
         await heroesToSelect()
     }   
 }
@@ -18,15 +27,18 @@ window.onload = async () => {
 if (document.querySelector("#deleteHeroSubmit"))
 document.querySelector("#deleteHeroSubmit").addEventListener("click", () => {
     deleteHero();
+    heroesSelect.remove(heroesSelect.selectedIndex)
 })
 
 if (document.querySelector("#editHeroSubmit"))
 document.querySelector("#editHeroSubmit").addEventListener("click", () => {
     editHero();
+    clearInputs();
 })
 
 if(document.querySelector("#addHeroSubmit"))
 document.querySelector("#addHeroSubmit").addEventListener("click", () => {
     addHero();
+    clearInputs();
 })
 
